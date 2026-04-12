2151ad6045a50b797cfdf85b2bb3ab87cfb9e6a0
wittns <mugishawitness15@gmail.com>
2026-04-12 22:36:43 +0200

    my  first ts project

---
+++ OneDrive/Documents/TSBASICS/hello.js
@@ -0,0 +1,6 @@
+"use strict";
+function greet(name) {
+    return `Hello, ${name} !`;
+}
+const message = greet("world");
+console.log(message);
---
+++ OneDrive/Documents/TSBASICS/hello.ts
@@ -0,0 +1,6 @@
+function greet(name: string): string {
+    return `Hello, ${name} !`
+}
+
+const message: string = greet("world");
+console.log(message)
