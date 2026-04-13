287c36fde800cdc8ac5cfcc79ca01a7d7f7013cf
wittns <mugishawitness15@gmail.com>
2026-04-13 22:10:53 +0200

    second commit

--- OneDrive/Documents/TSBASICS/hello.ts
+++ OneDrive/Documents/TSBASICS/hello.ts
@@ -2,5 +2,5 @@ function greet(name: string): string {
     return `Hello, ${name} !`
 }
 
-const message: string = greet("world");
-console.log(message)
+const msg: string = greet("world");
+console.log(msg)
